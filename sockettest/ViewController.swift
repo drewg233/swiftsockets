105001cf76eae7bbfee67d6608a19dd9ad8f88f5
Andrew Garcia <drewgarcia23@gmail.com>
2015-10-03 11:21:50 -0500

    Initial Commit

---
+++ sockettest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sockettest
+//
+//  Created by Drew Garcia on 10/3/15.
+//  Copyright © 2015 Drew Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
